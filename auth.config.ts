93f361883038713e83be52ed505379d994a04140
Khadijahmehmoodd <khadijamehmood77@gmail.com>
2025-06-23 19:33:56 +0500

    final-5

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,21 @@
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+    return user[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
 
 export const authConfig = {
   pages: {
@@ -16,7 +33,26 @@ export const authConfig = {
       return true;
     },
   },
-  // 👇 this satisfies TypeScript but is ignored in runtime
-  providers: [] as any,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsed = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (!parsed.success) return null;
+
+        const { email, password } = parsed.data;
+        const user = await getUser(email);
+        if (!user) return null;
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) return null;
+
+        return user;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
+
 
