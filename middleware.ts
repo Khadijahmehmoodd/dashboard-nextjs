105547d29c3dacbc5ddfbb940a9015c77099b9f8
Khadijahmehmoodd <khadijamehmood77@gmail.com>
2025-06-24 11:46:14 +0500

    final commit 5

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,18 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
- 
-export default NextAuth(authConfig).auth;
- 
-export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-};
+// import NextAuth from 'next-auth';
 // import { authConfig } from './auth.config';
-// import { auth } from './auth'; // make sure auth.ts exports `auth` like this: `export const { auth } = NextAuth(...)`
-
-// export default auth;
-
+ 
+// export default NextAuth(authConfig).auth;
+ 
 // export const config = {
+//   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 //   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 // };
+
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+
+export default NextAuth(authConfig).auth;
+
+export const config = {
+  matcher: ['/dashboard/:path*'], // 🔒 Protect only /dashboard and subroutes
+};
