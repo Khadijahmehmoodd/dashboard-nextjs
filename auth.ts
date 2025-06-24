c1113b20218786eec17a9247dfe1c09f4926d961
Khadijahmehmoodd <khadijamehmood77@gmail.com>
2025-06-24 11:29:00 +0500

    final commit 2

--- auth.ts
+++ auth.ts
@@ -1,14 +1,56 @@
+// import NextAuth from 'next-auth';
+// import { authConfig } from './auth.config';
+// import Credentials from 'next-auth/providers/credentials';
+// import { z } from 'zod';
+// import type { User } from '@/app/lib/definitions';
+// import bcrypt from 'bcrypt';
+// import postgres from 'postgres';
+ 
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+ 
+// async function getUser(email: string): Promise<User | undefined> {
+//   try {
+//     const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+//     return user[0];
+//   } catch (error) {
+//     console.error('Failed to fetch user:', error);
+//     throw new Error('Failed to fetch user.');
+//   }
+// }
+ 
+// export const { auth, signIn, signOut } = NextAuth({
+//   ...authConfig,
+//   providers: [
+//     Credentials({
+//       async authorize(credentials) {
+//         const parsedCredentials = z
+//           .object({ email: z.string().email(), password: z.string().min(6) })
+//           .safeParse(credentials);
+ 
+//         if (parsedCredentials.success) {
+//           const { email, password } = parsedCredentials.data;
+//           const user = await getUser(email);
+//           if (!user) return null;
+//           const passwordsMatch = await bcrypt.compare(password, user.password);
+//          if (passwordsMatch) return user;
+//         }
+//         console.log('Invalid credentials');
+//         return null;
+//       },
+//     }),
+//   ],
+// });
+
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
- 
+import { authConfig } from './auth.config';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
- 
+
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
@@ -18,8 +60,8 @@ async function getUser(email: string): Promise<User | undefined> {
     throw new Error('Failed to fetch user.');
   }
 }
- 
-export const { auth, signIn, signOut } = NextAuth({
+
+export const { auth, signIn, signOut, handlers } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
@@ -27,16 +69,20 @@ export const { auth, signIn, signOut } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
- 
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-          if (!user) return null;
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-         if (passwordsMatch) return user;
+
+        if (!parsedCredentials.success) {
+          console.log('❌ Invalid input format');
+          return null;
         }
-        console.log('Invalid credentials');
-        return null;
+
+        const { email, password } = parsedCredentials.data;
+        const user = await getUser(email);
+        if (!user) return null;
+
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (!passwordsMatch) return null;
+
+        return user;
       },
     }),
   ],
