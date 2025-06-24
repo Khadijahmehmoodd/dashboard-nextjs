43a26a190244f63e796514d536b622b0211628d7
Khadijahmehmoodd <khadijamehmood77@gmail.com>
2025-06-24 14:57:30 +0500

    final commit 8

--- app/api/[...nexthauth]/route.ts
+++
@@ -1,9 +0,0 @@
-export const runtime = 'nodejs';
-
-import NextAuth from 'next-auth';
-import { authConfig } from '@/auth.config';
-
-const handler = NextAuth(authConfig);
-
-export { handler as GET, handler as POST };
-
